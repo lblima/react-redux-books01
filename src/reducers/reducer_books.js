4a689aa3a2172deea0607666a920460fe06345f2
Leonardo Bruno Lima <leonardo.bruno@outlook.com>
2018-01-29 10:05:38 -0300

    .

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'book 01', pages: 101 },
+        { title: 'book 02', pages: 134 },
+        { title: 'book 03', pages: 1201 },
+        { title: 'book 04', pages: 11 }
+    ]
+}
